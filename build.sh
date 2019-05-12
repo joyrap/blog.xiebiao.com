720eb2a11523f8436ced8ff386ccbe27e89e6a33
xiebiao <joyrap@gmail.com>
2019-05-12 09:56:37 +0800

    update

--- build.sh
+++ build.sh
@@ -5,17 +5,21 @@ set -eux
 echo -e "Publish to blog.xiebiao.com!\n"
 
 rm -rf publish
+git submodule add git@github.com:joyrap/joyrap.github.io.git publish
 # Build the project. 
 hugo -d publish
 
-# git submodule add git@github.com:joyrap/joyrap.github.io.git docs
 # push docs to `joyrap.github.io`
 cp CNAME publish/
 cp -rf images publish/
 
 git add -A
+git commit -m "Update"
+git push 
+cd publish
+git add -A
 git commit -m "Publish"
-git push --recurse-submodules=check
+git push
 
 
 
