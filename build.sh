6af8acc437cd89e88a4016fe6dcc8284e91d9549
xiebiao <joyrap@gmail.com>
2019-07-09 00:30:51 +0800

    publish

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ echo -e "Publish to blog.xiebiao.com!\n"
 
 # git submodule add git@github.com:joyrap/joyrap.github.io.git publish
 # Build the project. 
+rm -rf publish
 hugo -d publish
+git submodule add git@github.com:joyrap/joyrap.github.io.git publish
 
 # push docs to `joyrap.github.io`
 cp CNAME publish/
