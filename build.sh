af8d089bb92500effd66e7485b8256558939023b
joyrap <xiebiao80@gmail.com>
2019-10-20 16:37:04 +0800

    Update

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo -e "Publish to blog.xiebiao.com!\n"
 # git submodule add git@github.com:joyrap/joyrap.github.io.git publish
 # Build the project. 
 rm -rf publish
-#git submodule add git@github.com:joyrap/joyrap.github.io.git publish
+#git submodule add git@joyrap.github.com:joyrap/joyrap.github.io.git publish
 hugo -d publish
 
 # push docs to `joyrap.github.io`
